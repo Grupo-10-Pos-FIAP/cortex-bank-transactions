538da812587009420743a509399bae27e4ad85d1
Gabrielle Martins <gabrielle.martiins.96@gmail.com>
2025-11-15 16:02:06 -0300

    chore: config webpack hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,31 @@ module.exports = (webpackConfigEnv, argv) => {
     outputSystemJS: false,
   });
 
+  const PORT = 3003;
+
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+        hot: true,
+        host: "0.0.0.0",
+        port: PORT,
+        allowedHosts: "all",
+        historyApiFallback: true,
+        watchFiles: ["src//", "public//"],
+  
+        client: {
+          webSocketURL: {
+            protocol: "ws",
+            hostname: "localhost",
+            port: PORT,
+          },
+        },
+      },
+  
+      watchOptions: {
+        poll: 1000,
+        aggregateTimeout: 300,
+        ignored: /node_modules/,
+      },
   });
 };
